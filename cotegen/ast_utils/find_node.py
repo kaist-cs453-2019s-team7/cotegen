cea2cfae9ca00be84a29834ac254b48862683293
greenmon <greenmon@kaist.ac.kr>
2019-06-02 10:10:36 +0900

    Separate find method

--- cotegen/ast_utils/find_node.py
+++ cotegen/ast_utils/find_node.py
@@ -1,33 +1,34 @@
 import ast
 import astor
 
+
 def find_function(node, name):
-    if isinstance(node, ast.FunctionDef) and node.name == name:
-        return node
+    def is_target_node(node, name): return isinstance(
+        node, ast.FunctionDef) and node.name == name
 
-    node = astor.iter_node(node)
-
-    for attr in node:
-        if attr[1] == 'body':
-            body = attr[0]
-
-            for child in body:
-                target = find_function(child, name)
-                if target:
-                    return target
+    return find(is_target_node, node, name)
 
 
 def find_assign(node, name):
-    if isinstance(node, ast.Assign) and node.targets[0].id == name:
+    def is_target_node(node, name): return isinstance(
+        node, ast.Assign) and node.targets[0].id == name
+
+    return find(is_target_node, node, name)
+
+
+def find(is_target_node, node, name):
+    if is_target_node(node, name):
         return node
 
     node = astor.iter_node(node)
 
     for attr in node:
-        if attr[1] == 'body':
-            body = attr[0]
+        if attr[1] != 'body':
+            continue
 
-            for child in body:
-                target = find_assign(child, name)
-                if target:
-                    return target
+        body = attr[0]
+
+        for child in body:
+            target = find(is_target_node, child, name)
+            if target:
+                return target
