9e55661c2cd2e5362d27de47012a791253234c7a
greenmon <greenmon@kaist.ac.kr>
2019-06-18 05:27:28 +0900

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,22 @@
-from setuptools import setup, find_packages
+import setuptools
 
-setup(name='cotegen',
-      version='0.1',
-      url='https://github.com/kaist-cs453-2019s-team7/cs453-team-project',
-      author='Team7',
-      author_email='suchan.park@kaist.ac.kr',
-      description='KAIST CS453 2019 Spring Team 7 Project',
-      packages=find_packages('cotegen'),
-      package_dir={'': 'cotegen'},
-      setup_requires=['astor>=0.8.0','anytree>=2.6.0'])
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='cotegen',
+    version='0.0',
+    url='https://github.com/kaist-cs453-2019s-team7/cs453-team-project',
+    author='Team7',
+    author_email='suchan.park@kaist.ac.kr',
+    description='KAIST CS453 2019 Spring Team 7 Project',
+    setup_requires=['astor>=0.8.0', 'anytree>=2.6.0'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
